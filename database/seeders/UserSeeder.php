f2131d2becfc0a5bad026c0b5251e4d327c7e9fc
Oliver Hickman <oliver.hickman@hey.com>
2022-03-12 17:31:08 -0500

    seed initial user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Camera;
 use App\Models\Roll;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,6 +17,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // Create 5 generic users
         User::factory()->count(5)->create();
+
+        // Create my user
+        User::create([
+            'name' => 'Oliver Hickman',
+            'email' => 'oliver.hickman@hey.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10)
+        ]);
     }
 }
